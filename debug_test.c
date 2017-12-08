dab5b5be9b97f3a4ee2c6ee65e53fe4be0d2dff4
Thomas Hornschuh <thomas@hornschuh-family.de>
2017-12-08 17:33:18 +0100

    Small changes

--- debug_test.c
+++ debug_test.c
@@ -10,6 +10,46 @@ void do_increment(volatile int *px)
 
 #define BAUDRATE 115200
 
+#define SIZE 10
+
+uint64_t  test[SIZE];
+
+void initArray()
+{
+int i;
+
+  for(i=0;i<SIZE;i++) test[i]=i*i;
+
+}
+
+
+void insertLastAt(int pos)
+// Inserts Last element of the array at pos
+{
+
+uint64_t *p;
+
+   for(p=&test[SIZE-1];p>&test[pos];p--) {
+       *p = *(p-1);
+   }
+   test[pos]=test[SIZE-1];
+}
+
+
+
+void arrayTest()
+{
+int i;
+
+  initArray();
+  for(i=0;i<SIZE;i++) printk("%4llu ",test[i]);
+  insertLastAt(3);
+  printk("\n");
+  for(i=0;i<SIZE;i++) printk("%4llu ",test[i]);
+  gdb_breakpoint();
+}
+
+
 void main()
 {
 volatile int i=0;
@@ -17,11 +57,9 @@ volatile int i=0;
   printk("Run with baudrate %d\n",BAUDRATE);
   gdb_setup_interface(BAUDRATE);
   gdb_initDebugger(1);
-  gdb_breakpoint();
 
 
-
-
+  arrayTest();
 
   while(1) {
 
